95473a69a59e352210c868fbfcf35d60b364dd3d
Jules Genin <jules.genin@epita.fr>
2026-04-28 11:38:18 +0200

    feat: Add movie banner

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -4,13 +4,16 @@ import {Observable} from 'rxjs';
 import {Movie} from '../models/movie';
 import {AsyncPipe, DatePipe} from '@angular/common';
 import {MovieCard} from './movie-card/movie-card';
+import {map} from 'rxjs/operators';
+import { MovieBanner } from './movie-banner/movie-banner';
 
 @Component({
   selector: 'app-home',
   imports: [
     AsyncPipe,
     DatePipe,
-    MovieCard
+    MovieCard,
+    MovieBanner
   ],
   templateUrl: './home.html',
   styleUrl: './home.scss',
@@ -18,4 +21,7 @@ import {MovieCard} from './movie-card/movie-card';
 export class Home {
   private readonly moviesApi = inject(MoviesApi)
   movies$: Observable<Movie[]> = this.moviesApi.getMovies()
+  randomMovie$: Observable<Movie | undefined> = this.movies$.pipe(
+    map(movies => movies.length > 0 ? movies[Math.floor(Math.random() * movies.length)] : undefined)
+  )
 }
---
+++ src/app/home/movie-banner/movie-banner.ts
@@ -0,0 +1,18 @@
+import {Component, Input} from '@angular/core';
+import {Movie} from '../../models/movie';
+
+@Component({
+  selector: 'app-movie-banner',
+  imports: [],
+  templateUrl: './movie-banner.html',
+  styleUrl: './movie-banner.scss',
+})
+export class MovieBanner {
+  @Input({required : true}) movie! : Movie
+
+  isOvered = false;
+
+  toggleOvered() {
+    this.isOvered = !this.isOvered;
+  }
+}

c670f47eab0b71c0992fa5932f8f52a91503d579
Nino Govin <thundersilver85@gmail.com>
2026-04-28 14:56:25 +0200

    doc: rename app

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# TakimaCiné
+# AloCinoche
 
 ## Development server
 
--- src/app/app.ts
+++ src/app/app.ts
@@ -10,5 +10,5 @@ import { Footer } from './footer/footer';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('TakimaCiné');
+  protected readonly title = signal('AloCinoche');
 }
